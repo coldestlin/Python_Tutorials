f85089b6d42e494c300b83024c35cf529df38e3a
Gee <coldestlin@qq.com>
2025-04-08 14:35:28 +0800

    Update test0408.py

--- test0408.py
+++ test0408.py
@@ -1,4 +1,23 @@
 print("bbbb")
 
 import os
+
+print("---\n")
+
 print(os.system("ls -alh"))
+
+print("----\n")
+
+print(os.system("df -h"))
+print("----\n")
+
+print(os.system("pwd"))
+
+print("--- check env\n")
+
+print(os.system("env"))
+
+
+print("--- call other py")
+
+python datatest.py
