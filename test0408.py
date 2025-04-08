b6d759912e361a79653f1a7ee1a6d116fd7783d1
Gee <coldestlin@qq.com>
2025-04-08 15:01:39 +0800

    Update test0408.py

--- test0408.py
+++ test0408.py
@@ -18,6 +18,18 @@ print("--- check env\n")
 print(os.system("env"))
 
 
+print("--check file again\n")
+
+print(os.system("ls -alh /Workspace/Repos/"))
+
+print("--check internal \n")
+
+print(os.system("ls -alh /Workspace/Repos/.internal"))
+
+
+
 print("--- call other py")
+
+
 
 python ./datatest.py
